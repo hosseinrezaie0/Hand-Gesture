596d3f6b308dbf8b19720951fd98643bf1deb793
hosseinrezaie0 <hossein.rezaie81@gmail.com>
2024-06-19 18:37:55 +0330

    Open webcam

--- Project.py
+++ Project.py
@@ -8,6 +8,27 @@ mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(min_detection_confidence = 0.8, min_tracking_confidence = 0.8)
 mp_drawing = mp.solutions.drawing_utils
 
+# Open webcam
+cam = cv2.Videocamture(0)
+
+while cam.isOpened():
+    ret, frame = cam.read()
+    if not ret:
+        break
+    
+    frame = cv2.flip(frame, 1)
+    # Convert the frame from BGR (opencv channel) to RGB
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    cv2.imshow('Window', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+
+cam.release()
+cv2.destroyAllWindows()
+
 
 
 
