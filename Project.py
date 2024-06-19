ff92e6184b6a6c5da18abadc18f460adabd2d49f
hosseinrezaie0 <hossein.rezaie81@gmail.com>
2024-06-19 18:09:29 +0330

    Initialize MediaPipe Hands

--- Project.py
+++ Project.py
@@ -2,3 +2,12 @@
 import cv2
 import mediapipe as mp
 
+
+# Create Mediapipe objects
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(min_detection_confidence = 0.8, min_tracking_confidence = 0.8)
+mp_drawing = mp.solutions.drawing_utils
+
+
+
+
