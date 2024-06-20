a8512c0f4ef97336b4b5dc6bab60ad0a7f4475c8
hosseinrezaie0 <hossein.rezaie81@gmail.com>
2024-06-20 21:19:35 +0330

    Count fingers when direction is right

--- Project.py
+++ Project.py
@@ -93,6 +93,34 @@ def count_fingers(result, frame, dir):
                     else:
                         fingers.append(0)
 
+        if dir == "Right":
+            fingers = []
+            for idx, landmark in enumerate(hand_landmark.landmark):
+                if idx == 4:  # Thumb
+                    if (landmark.y < hand_landmark.landmark[3].y):
+                        fingers.append(1)
+                    else:
+                        fingers.append(0)
+                if idx == 8:  # Index
+                    if (landmark.x > hand_landmark.landmark[6].x):
+                        fingers.append(1)
+                    else:
+                        fingers.append(0)
+                if idx == 12:  # Middle
+                    if (landmark.x > hand_landmark.landmark[10].x):
+                        fingers.append(1)
+                    else:
+                        fingers.append(0)
+                if idx == 16:  # Ring
+                    if (landmark.x > hand_landmark.landmark[14].x):
+                        fingers.append(1)
+                    else:
+                        fingers.append(0)
+                if idx == 20:  # Pinky
+                    if (landmark.x > hand_landmark.landmark[18].x):
+                        fingers.append(1)
+                    else:
+                        fingers.append(0)
 
 
 
